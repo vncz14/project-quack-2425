0ad20eafa9e05192a0822fcc11817c59feb4c2fb
vincenzo-sorcigli <57498723+vincenzo-sorcigli@users.noreply.github.com>
2024-10-10 20:06:35 -0400

    oops this should have revalidation

--- frontend/src/app/event/ExampleEventList.tsx
+++ frontend/src/app/event/ExampleEventList.tsx
@@ -3,7 +3,9 @@ import { Text } from "@/components/ui/text";
 import Link from "next/link";
 
 const ExampleEventList = async () => {
-    const res = await fetch(`${process.env.API_URL}/event`);
+    const res = await fetch(`${process.env.API_URL}/event`, {
+        next: { revalidate: 10 },
+    });
     const events = await res.json();
     return events.map(
         (event: { id: number; name: string } /*TODO: CHANGE*/) => (
