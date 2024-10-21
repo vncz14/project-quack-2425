292cfe7002b9218af1ef33df19bba8b6c2599703
pixel-pine <video_derby0f@icloud.com>
2024-10-21 12:39:24 -0400

    fixed a couple things

--- backend/friends_app_api/models.py
+++ backend/friends_app_api/models.py
@@ -3,6 +3,27 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=30, null=False)
+    description = models.CharField(max_length=255, null=True)
+    interestTags = 
+    publicStatus = models.BooleanField () #true=public, false=private
+    maxCapacity = models.IntegerField()
+    invites =
+    location =
+    eventType =
+    eventDate = models.DateTimeField()
+
 
 
 # Create your models here.
+class Users(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
+    major = [
+    ('CSE', 'Computer Science'),
+    ('AMS', 'Applied Mathematics and Statistics'),
+    ('BIO', 'Biology'),
+    ]
+    interests = 
+    points = models.IntegerField()
+    eventsWentTo = #would this be integer, char, or something else?
