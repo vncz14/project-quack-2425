e45cb08f90c79672297e63f1439f872a36847260
Jian De Mai <jiande.mai@stonybrook.edu>
2024-10-21 20:39:01 -0400

    default layout using shadcn/ui library

--- frontend/src/components/ui/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer () {
-  return (
-    <div className="p-3">
-      <p>Project Quack 24-25</p>
-      <p>Featuring: Mentor Vince. Mentees Riley, Joseph, Jian, Shahan</p>
-    </div>
-  )
-}
