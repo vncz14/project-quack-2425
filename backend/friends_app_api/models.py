697373e0ecce4a7a2670fe37c47daedf21dd866f
pixel-pine <video_derby0f@icloud.com>
2024-10-27 15:48:05 -0400

    Basic models completed except invite and calendar

--- backend/friends_app_api/models.py
+++ backend/friends_app_api/models.py
@@ -1,35 +1,36 @@
 from django.db import models
 
-# Need to input choices (using json file maybe??) and check docs to make sure I'm doing it correctly
-
 class Event(models.Model):
     name = models.CharField(max_length=30, null=False)
-    description = models.CharField(max_length=255, null=True)
-    interestTags = models.CharField(choices=[])
-    maxCapacity = models.IntegerField()
-    invites = # manytomanyfield of a new custom invite model
-    location = models.CharField(choices=[])
-    eventType = models.CharField(choices=["Social", "Bike", "Walk"]) # add other types
+    description = models.CharField(max_length=255, null=True, editable=True)
+    interestTags = models.ForeignKey(Users.interests)
+    maxCapacity = models.PositiveIntegerField(max_length=3)
+    '''invites = manytomanyfield of a new custom invite model'''
+    location = models.CharField(choices=locationChoices)
+    eventType = models.CharField(choices=[('S', 'Social'), ('B', 'Bike'), ('W', 'Walk')]) # add other types later
     eventDate = models.DateTimeField()
     owner = models.ForeignKey(Users)
-    publicStatus = models.CharField(choices=["Public", "Private"])
-    #chats = 
-
+    publicStatus = models.CharField(choices=[('PUB','Public'), ('PRI','Private')])
+    locationChoices = [] #finish inputting
+    # chats = 
 
 class Users(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
     bio = models.CharField(max_length=255)
-    major = models.Charfield(choices=[])
-    interests = models.CharField(choices=[])
-    clubs = models.CharField(choices=[])
+    major = models.Charfield(choices=majorChoices)
+    interests = models.CharField(choices=interestChoices)
+    clubs = models.CharField(choices=clubChoices)
     points = models.PositiveIntegerField()
-    eventsWentTo = # make a computed property
-    residentialStatus = models.CharField(choices=["Residential", "Commuter"])
-    calendar = #foreignkey to a calendar model (ISSUE)
-    standing = models.CharField(choices=["Freshman", "Sophomore", "Junior", "Senior"]) # also not in doc, should we include grad students?
+    '''eventsWentTo = make a computed property'''
+    residentialStatus = models.CharField(choices=[('RES', 'Residential'), ('COM', 'Commuter')])
+    '''calendar = foreignkey to a calendar model (ISSUE)'''
+    standing = models.CharField(choices=[('FRE', 'Freshman'), ('SOP', 'Sophomore'), ('JUN', 'Junior'), ('SEN', 'Senior')])
+    majorChoices = [
+        ('AFS', 'Africana Studies'),
+        ('ANT', 'Anthropology'),
+        ('AMS', 'Applied Mathematics and Statistics')
+    ] # finish inputting majors and major codes
+    clubChoices = [] # finish inputting
+    interestChoices = [] # finish inputting
     # work on google auth stuff
-
-    # stretch goal: items for wolfie
-
-# stretch goals: invite class and chat message class
