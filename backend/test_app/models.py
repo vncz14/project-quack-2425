4f22c016f3b8a2d10daa1a4b619bedc8b300296f
pixel-pine <video_derby0f@icloud.com>
2025-01-08 20:14:13 -0500

    Implementation of Location, Major, Club Complete

--- backend/test_app/models.py
+++ backend/test_app/models.py
@@ -1,36 +1,45 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.core.exceptions import ValidationError
+import re
+
+def validate_username(value): # Checks to make sure usernames do not contain invalid characters
+    if not re.match('^[a-zA-Z0-9_]+$', value):
+        raise ValidationError('Invalid username')
 
 # Create your models here.
 
-class Location(models.Model): #How can I make sure only one location is chosen?
+class Location(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self): # makes location into a human readable format in admin interface
+    return self.name
+
+class Major(models.Model):
   name = models.CharField(max_length=255)
 
   def __str__(self):
     return self.name
 
-class Major(models.Model): #How can I make sure no more than 2 majors are chosen?
-  name = models.CharField(max_length=255)
-
-  def __str__(self):
-    return self.name
-
-class Club(models.Model): #How can I make sure a limit of ~10 clubs are chosen?
+class Club(models.Model):
   name = models.CharField(max_length=255)
 
   def __str__(self):
     return self.name
 
 class User(models.Model):
-  username = models.CharField(max_length=30, null=False)
-  major = models.ForeignKey(Major, on_delete=models.SET_NULL, null=True)
-  club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True) # associated clubs/clubs they are in
+  username = models.CharField(max_length=30, null=False, unique=True, validators=[validate_username])
+  major = models.ManyToManyField(Major, blank=True)
+  club = models.ManyToManyField(Club, blank=True) # associated clubs/clubs they are in
+  bio = models.CharField(max_length=255, null=True, editable=True, blank=True)
+  points = models.PositiveIntegerField(default=0)
+  residentialStatus = models.CharField(choices = [('RES', 'Residential'), ('COM', 'Commuter')], max_length = 255, default = 'Residental')
+  standing = models.CharField(choices = [('FRE', 'Freshman'), ('SOP', 'Sophomore'), ('JUN', 'Junior'), ('SEN', 'Senior')], max_length = 255, default = 'Freshman')
   def greeting(self):
     return "Hello " + self.username
 
   def __str__(self):
     return self.username
-  
 
 class Event(models.Model):
   eventName = models.CharField(max_length=30, null=False)
@@ -39,7 +48,7 @@ class Event(models.Model):
   maxCapacity = models.PositiveIntegerField(validators=[MaxValueValidator(250)])
   eventDate = models.DateTimeField(null=False)
   publicStatus = models.CharField(max_length=30, choices=[('OPEN','Open'), ('CLOSED','Closed')])
-  location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True) #on_delete=models.SET_NULL means if a Location is deleted from databse, the Event will still exist but won't have any value in location field
+  location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True) # on_delete=models.SET_NULL means if a Location is deleted from databse, the Event will still exist but won't have any value in location field
 
   def __str__(self):
     return self.eventName
