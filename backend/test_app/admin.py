1654f0d28130514c014421565162980c4efa39ea
pixel-pine <video_derby0f@icloud.com>
2025-01-11 15:26:26 -0500

    Invite Model Created

--- backend/test_app/admin.py
+++ backend/test_app/admin.py
@@ -5,4 +5,5 @@ from . import models
 
 admin.site.register(models.User)
 admin.site.register(models.Event)
+admin.site.register(models.Invite)
 
--- backend/test_app/models.py
+++ backend/test_app/models.py
@@ -7,7 +7,6 @@ class User(models.Model):
   username = models.CharField(max_length=30, null=False)
   def greeting(self):
     return "Hello " + self.username
-  
 
 class Event(models.Model):
   eventName = models.CharField(max_length=30, null=False)
@@ -16,11 +15,18 @@ class Event(models.Model):
   maxCapacity = models.PositiveIntegerField(validators=[MaxValueValidator(250)])
   eventDate = models.DateTimeField(null=False)
   publicStatus = models.CharField(max_length=30, choices=[('OPEN','Open'), ('CLOSED','Closed')])
+  invites = models.ManyToManyField(User, through='Invite', related_name='events_invited_to', through_fields=('event', 'receiver'))
 
   # TAGS
-  # interestTags = models.ForeignKey(Users.interests)
+  # interestTags = modecls.ForeignKey(Users.interests)
   eventTags = models.CharField(max_length=30, default='OTHER', choices=[
     ('SOCIAL','Social'), ('SPORT','Sport'), ('HOBBY','Hobby'), ('OTHER', 'Other')
     ])
   
+class Invite(models.Model):
+  sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_invites')
+  receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_invites')
+  event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
+  def __str__(self):
+    return f'Invite from {self.sender} to {self.receiver} for {self.event}'
